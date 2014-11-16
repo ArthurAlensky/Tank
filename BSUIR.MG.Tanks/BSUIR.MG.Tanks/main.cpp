06784e77d485b85ca306243b08be3a68dd169061
ArthurAlensky <arthuralensky@gmail.com>
2014-11-16 12:52:47 +0200

    Fix Build

--- BSUIR.MG.Tanks/BSUIR.MG.Tanks/main.cpp
+++ BSUIR.MG.Tanks/BSUIR.MG.Tanks/main.cpp
@@ -187,44 +187,6 @@ void myInit()
 	glFogf(GL_FOG_END, 1000);
 	glFogf(GL_FOG_DENSITY, gFogDensity);
 
-	// The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
-	 GLuint FramebufferName = 0;
-	 glGenFramebuffers(1, &FramebufferName);
-	 glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
- 
-	 // Depth texture. Slower than a depth buffer, but you can sample it later in your shader
-	 GLuint depthTexture;
-	 glGenTextures(1, &depthTexture);
-	 glBindTexture(GL_TEXTURE_2D, depthTexture);
-	 glTexImage2D(GL_TEXTURE_2D, 0,GL_DEPTH_COMPONENT16, 1024, 1024, 0,GL_DEPTH_COMPONENT, GL_FLOAT, 0);
-	 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
- 
-	 glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthTexture, 0);
- 
-	 glDrawBuffer(GL_NONE); // No color buffer is drawn to.
- 
-	 // Always check that our framebuffer is ok
-	 if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-		exit(0);
-
-	 vec3 lightInvDir = vec3(0.5f,2,2);
- 
-	 // Compute the MVP matrix from the light's point of view
-	 mat4 depthProjectionMatrix = GLOrthographic(-10,10,-10,10,-10,20);
-	 mat4 depthViewMatrix = GLLookAt(lightInvDir, vec3(0,0,0), vec3(0,1,0));
-	 mat4 depthModelMatrix = mat4(1.0,1.0,1.0,1.0,
-									1.0,1.0,1.0,1.0,
-									1.0,1.0,1.0,1.0,
-									1.0,1.0,1.0,1.0);
-	 mat4 depthMVP = depthProjectionMatrix * depthViewMatrix * depthModelMatrix;
- 
-	 // Send our transformation to the currently bound shader,
-	 // in the "MVP" uniform
-	 glUniformMatrix4fv(depthMatrixID, 1, GL_FALSE, &depthMVP[0][0])
-
 	//initial terrain
 	myTerrain.initializeTerrain("E:/My_Tank/Work/Tank/BSUIR.MG.Tanks/Data/Texture/Terrain/sand.tga", "E:/My_Tank/Work/Tank/BSUIR.MG.Tanks/Data/Texture/Terrain/cactus.tga");
 
@@ -236,7 +198,6 @@ void myInit()
 	tankHeightPos = myTerrain.GetHeight(gCenterPoint.X()+100.0, gCenterPoint.Z());
 
 	myTank.setParameters("T-90.3DS", "E:/My_Tank/Work/Tank/TANK-T90/T-90/",TVector(gCenterPoint.X()+100.0, tankHeightPos + 9.0, gCenterPoint.Z()), 0.0, &myTerrain, &collisionBoxArray, &wall);
-	//myTank.setParameters("E:/My_Tank/Work/Tank/Tank-34/111.3DS", TVector(gCenterPoint.X()+100.0, tankHeightPos + 9.0, gCenterPoint.Z()), 0.0, &myTerrain, &collisionBoxArray);
 	myTank.initTank();	
 
 	unsigned int brickTexture;
@@ -244,9 +205,6 @@ void myInit()
 	createTexture("E:/My_Tank/Work/Tank/BSUIR.MG.Tanks/Data/Texture/Brick/brick.bmp", brickTexture);
 
 	wall.Init(myTank.getPosition().X() + 50, myTank.getPosition().Y(), myTank.getPosition().Z() + 20, NUM_BRICKS, brickTexture, &myTerrain);
-	/*gCameraPosition.setX(myTank.getPosition().X() + 150*cos(gRadViewAngle));
-	gCameraPosition.setY(myTank.getPosition().Y() + 30);
-	gCameraPosition.setZ(myTank.getPosition().Z() - 150*sin(gRadViewAngle));*/
 
 	myTank.startFight();
 	mySetLight();
